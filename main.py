c06a447afbee510fa986a2398642d7767f447e14
TahaThinks <tahaa4587@gmail.com>
2024-04-21 23:31:04 +0400

    Taha, updated 21/04/2024

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import requests
 
 iss_location = "http://api.open-notify.org/iss-now.json"
 response = requests.get(url=iss_location)
-print(response)
+
+# Check Errors in API
+if response.status_code == 404:
+    raise Exception("That resource does not exist.")
+elif response.status_code == 401:
+    raise Exception("You are not Authorized")
+
 

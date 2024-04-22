24c65396f5151788c3a996dd21f4bb9b8a283714
TahaThinks <tahaa4587@gmail.com>
2024-04-23 02:38:15 +0400

    Edit to UTC +04:00

--- sunrise_sunset.py
+++ sunrise_sunset.py
@@ -1,12 +1,17 @@
 import requests
+from datetime import datetime
 
-MY_LAT = 25.204849
-MY_LON = 55.270782
+MY_LAT = 25.276987
+MY_LON = 55.296249
+FORMAT = 0
+TIMEZONE = "Asia/Muscat"
 
 sunset_sunrise_url = "https://api.sunrise-sunset.org/json"
 my_parameters = {
     "lat": MY_LAT,
     "lng": MY_LON,
+    "formatted": FORMAT,
+    "tzid": TIMEZONE
 }
 response = requests.get(url=sunset_sunrise_url, params=my_parameters)
 
@@ -14,7 +19,7 @@ response = requests.get(url=sunset_sunrise_url, params=my_parameters)
 response.raise_for_status()
 
 # Check Status Code
-print(response.status_code)
+# print(response.status_code)
 
 # Get the Data
 data = response.json()["results"]
@@ -22,4 +27,7 @@ data = response.json()["results"]
 sunrise = data["sunrise"]
 sunset = data["sunset"]
 
-print(data)
+print(f"sunrise: {sunrise}\nsunset: {sunset}")
+
+time_now = datetime.now()
+print(time_now)

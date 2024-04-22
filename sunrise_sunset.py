06c07e3427b55a7e5f99e943d0b52cc85a039dcb
TahaThinks <tahaa4587@gmail.com>
2024-04-23 02:13:34 +0400

    Add Parameters Value

--- sunrise_sunset.py
+++ sunrise_sunset.py
@@ -1,6 +1,10 @@
 import requests
 
 sunset_sunrise_url = "https://api.sunrise-sunset.org/json"
+parameters = {
+    "lat": 25.204849,
+    "lon": 55.270782,
+}
 response = requests.get(url=sunset_sunrise_url)
 
 # Raise if issues
